f743b802850b85a58af027cac5634626bf3fb15c
Saksham kohli <saksham.kohli@marblex.ai>
2025-05-12 18:18:42 +0530

    fix: changed export statement of refund.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 
 export { isETABreached } from "./eta.js";
 export { isCancellable } from "./cancelability.js";
-export { calculateRefundAmount } from "./refund.js"
+export { calculateRefundAmount, refund } from "./refund.js"
 
